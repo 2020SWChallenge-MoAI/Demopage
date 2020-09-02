65b00ff8ba337154045460d327d142d00990b5e6
Heekang Park <park.heekang33@gmail.com>
2020-09-03 01:59:20 +0900

    express hello world

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World');
-}).listen(8888, '172.26.0.1');
-console.log('Server running at http://172.26.0.1:8888/');
+var express = require('express');
+var app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(8888, "172.26.0.1", () => {
+  console.log("Example app listening at http://172.26.0.1:8888");
+})
